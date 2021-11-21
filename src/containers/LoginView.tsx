fab5c459f4a4876c4bf5b47ec0b4bfd940eb9d43
Karol Kosek <karolkos@student.agh.edu.pl>
2021-11-21 14:13:39 +0100

    adding logo

--- src/containers/LoginView.tsx
+++ src/containers/LoginView.tsx
@@ -2,28 +2,26 @@ import LoginForm from 'components/LoginForm';
 
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-
+import { ReactComponent as DeploygramLogo } from 'images/deploygram-logo.svg';
 
 const LoginView = () => (
+    <Container sx={{ width: '100vw', height: '90vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <Card sx={{ width: 350 }}>
             <CardContent>
                 <Box 
                     sx={{ display: 'flex', flexDirection: 'column'}} 
                     component="div"
                 >
-                    <Typography 
-                        sx={{ p: 3, textAlign: 'center' }} 
-                        component="h1" 
-                        fontSize="large"
-                    >
-                        Deploygram
-                    </Typography>
+                    <Box sx={{ display: 'flex', justifyContent: 'center', m: 4 }}>
+                        <DeploygramLogo />
+                    </Box>
                     <LoginForm />
                 </Box>
             </CardContent>
         </Card>
+    </Container>
     );
 
 export default LoginView
