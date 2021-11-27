922f50fa5714f89ee74f8b060f1733e1e513a7ee
Karol Kosek <karolkos@student.agh.edu.pl>
2021-11-27 16:20:39 +0100

    fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,37 @@
 import Stack from '@mui/material/Stack';
-import Storiesarray from "components/Storiesarray";
-import PostPreviewTimeline from 'components/PostPreviewTimeline'
+import Storiesarray from 'components/Storiesarray';
+import PostPreviewTimeline from 'components/PostPreviewTimeline';
 import { useEffect, useState } from 'react';
+import Sidebar from '../components/sidebar/SideBar';
+import Comment from '../components/Comment';
 
 const Home = () => {
-  const [profileData, setProfileData] = useState(null);
-  const [posts, setPosts] = useState(null);
+    const [profileData, setProfileData] = useState(null);
+    const [posts, setPosts] = useState(null);
 
-  useEffect(() => {
-    fetch('https://deploygram.deployed.space/posts')
-      .then(res => res.json())
-      .then((res) => {
-        setPosts(res);
-      });
-  }, [])
+    useEffect(() => {
+        fetch('https://deploygram.deployed.space/posts')
+            .then(res => res.json())
+            .then((res) => {
+                setPosts(res);
+            });
+    }, []);
 
-  return (
-    <Stack direction="row" spacing={2}>
-      <Stack flex={1}>
-        <Storiesarray/>
-        Timeline
-        {posts == null ? null : posts.map((item, i) => {
-          return <PostPreviewTimeline username={item.userId} images={item.images} key={i} />
-        })}
-         <Comment userName="Name" text="My new very very very long comment" creationDate={new Date("November 27, 2021 12:15:00")} imgUrl={dog}></Comment>
-      </Stack>
-      <Stack width="300px">
-        <Sidebar />
-      </Stack>
-    </Stack>
-  );
+    return (
+        <Stack direction="row" spacing={2}>
+            <Stack flex={1}>
+                <Storiesarray/>
+              Timeline
+                {posts == null ? null : posts.map((item, i) => {
+                    return <PostPreviewTimeline username={item.userId} images={item.images} key={i} />;
+                })}
+                <Comment userName="Name" text="My new very very very long comment" creationDate={new Date('November 27, 2021 12:15:00')} imgUrl={dog}></Comment>
+            </Stack>
+            <Stack width="300px">
+                <Sidebar />
+            </Stack>
+        </Stack>
+    );
 };
 
 export default Home;
