675c446b625cf30f09c20a57e4516db2cd4805a8
qcianna <annaqcia@gmail.com>
2021-11-27 14:41:06 +0100

    comment_component_fixed

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import Avatar from '@mui/material/Avatar';
-import dog from "images/dog.jpg";
 import "./comment.css";
 
+function Comment({userName, text, creationDate, imgUrl}) {
 
-function Comment({userName, text, creationDate}) {
+    var day = creationDate.getDate();
+    var month = creationDate.getMonth();
+    var year = creationDate.getFullYear();
+    var h = creationDate.getHours();  
+    var m = creationDate.getMinutes();  
 
     return (
         <>
             <div className="wrapper">
-                <Avatar alt="dog" src={dog} />
+                <Avatar alt="dog" src={imgUrl} />
                 <div>
                     <div className="wrapper">
                         <p className="user">{userName}</p>
                         <p className="text">{text}</p>
                     </div>
-                    <p>{creationDate}</p>
+                    <p className="date">{day + "-" + month + "-" + year + " " + h + "h " + m + "min"}</p>
                 </div>
             </div>
         </>
