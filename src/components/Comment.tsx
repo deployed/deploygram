c78b9997cbf39efeeea3314b85a7b1320835737d
Yiome <yiome@outlook.com>
2021-11-27 12:49:18 +0100

    unused AllComments removed

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -18,7 +18,6 @@ export function Comment(props: any) {
         margin: "12px",
         boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
         display: "flex",
-        // flexDirection: "column"
     }
 
     const marginStyle = {
@@ -38,38 +37,6 @@ export function Comment(props: any) {
                 <div style={{marginBottom: "8px"}}><i>{createdAt.toDateString()}</i></div>
                 <div>{text}</div>
             </div>
-        </div>
-    );
-}
-
-
-export function AllComments() {
-
-    const [comments, setComments] = useState([1, 2, 4]);
-
-    useEffect(() => {
-        // fetch("https://deploygram.deployed.space/comments").then(response => response.json()).then(arr => setComments(arr));
-        // fetch("https://deploygram.deployed.space/comments").then(response => response.json()).then(arr => console.log(arr));
-    }, []);
-
-    const mockup = {
-        avatar:"https://www.ckziu.jaworzno.pl/wp-content/uploads/2016/09/no-user-image.gif",
-        username:"some username",
-        text:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable",
-        creationDate:"2021-11-27T09:34:56.766Z"
-    };
-
-    return (
-        <div>
-            {
-                comments.map(x => {
-                    return (
-                        <Comment key={0} {...mockup}>
-                            
-                        </Comment>
-                    );
-                })
-            }
         </div>
     );
 }
