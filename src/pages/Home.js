d1c4d3eb5a1e5efd3b15d2132f7d6a0e18c55a97
Marcin Papież <mmpapiez@gmail.com>
2021-11-27 16:01:57 +0100

    19 First commit (#51)
    * 19 First commit
    
    * contact with backend
    
    Co-authored-by: 5ZYSZ3K <paw.por@wp.pl>
    Co-authored-by: Karol Kosek <56894071+karolkos-glitch@users.noreply.github.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,21 @@
-import Stack from '@mui/material/Stack';
+import Stack from "@mui/material/Stack";
+import ThumbnailGrid from "components/PostComponents/ThumbnailGrid";
 
 import SideBar from '../components/sidebar/SideBar.js';
 
 const Home = () => {
-    return (
-        <Stack direction="row" spacing={2}>
-            <Stack flex={1}>
-                {/* This is the place for story list and post list */}
-                Timeline
-            </Stack>
-            <Stack width="300px">
-                <SideBar/>
-                {/* All Sidebar related things go here */}
-                Sidebar
-            </Stack>
-        </Stack>
-    );
+  return (
+    <Stack direction="row" spacing={2}>
+      <Stack flex={1}>
+        {/* This is the place for story list and post list */}
+        Timeline
+      </Stack>
+      <Stack width="300px">
+        {/* All Sidebar related things go here */}
+        Sidebar
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Home;
