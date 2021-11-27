839b046e320f5689fadc5c6bfe4664772c079a81
Tavczan <jakub.kawka.2001@gmail.com>
2021-11-27 14:46:25 +0100

    Adds story component and fixes from previous PR

---
+++ src/components/Story/Story.js
@@ -0,0 +1,17 @@
+import { StyledStack, StyledAvatar, StyledTypography } from './styles';
+
+export const Story = ( { image, username } ) => {
+    if (username?.length >= 10)
+    {
+        username = username.substring(0, 8) + '...';
+    }
+
+    return (
+        <StyledStack>
+            <StyledAvatar alt="image" src={ image } ></StyledAvatar>
+            <StyledTypography>{ username }</StyledTypography>
+        </StyledStack>
+    );
+};
+
+export default Story;
