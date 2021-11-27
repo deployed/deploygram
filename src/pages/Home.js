2545bdb953e0d484ef1023463608b7823ccb006a
BinarySoftware <mkmikolajek@icloud.com>
2021-11-27 10:25:12 +0100

    save

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import Stack from '@mui/material/Stack';
+import PhotoPost from 'components/PhotoPost';
 
 const Home = () => {
     return (
@@ -6,6 +7,7 @@ const Home = () => {
             <Stack flex={1}>
                 {/* This is the place for story list and post list */}
                 Timeline
+                <PhotoPost image="https://upload.wikimedia.org/wikipedia/commons/4/4b/FoosLondonStad220618-124_%2842989552522%29.jpg"/>
             </Stack>
             <Stack width="300px">
                 {/* All Sidebar related things go here */}
