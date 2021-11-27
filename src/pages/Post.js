3afe07160d121c6cdb1ef2bc37d195d0cb13ad9b
HuskyyTrzaski <kubajochymek@gmail.com>
2021-11-27 14:25:58 +0100

    #34 and #35 fixed

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,15 +1,27 @@
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
+import SinglePostPhoto from "../components/SinglePostPhoto";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
 
 const Post = () => {
-    const params = useParams();
+  const params = useParams();
 
-    console.log(params);
+  console.log(params);
 
-    return (
-        <div>
-            Post page
-        </div>
-    );
+  return (
+    <>
+      <Box>
+        <Stack flexDirection="row">
+          <Box
+            sx={{ flex: 1, aspectRatio: 1, width: "625px", height: "625px" }}
+          >
+            <SinglePostPhoto />
+          </Box>
+          <Stack width="340px">Post info, comment list ect.</Stack>
+        </Stack>
+      </Box>
+    </>
+  );
 };
 
 export default Post;
