36e2517b5444ae537f0970dbd402f500721c0937
qcianna <annaqcia@gmail.com>
2021-11-27 16:15:43 +0100

    save

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import Stack from '@mui/material/Stack';
+import Comment from "../components/Comment"
+import dog from "images/dog.jpg";
 
 const Home = () => {
     return (
@@ -6,6 +8,7 @@ const Home = () => {
             <Stack flex={1}>
                 {/* This is the place for story list and post list */}
                 Timeline
+                <Comment userName="Name" text="My new very very very long comment" creationDate={new Date("November 27, 2021 12:15:00")} imgUrl={dog}></Comment>
             </Stack>
             <Stack width="300px">
                 {/* All Sidebar related things go here */}
