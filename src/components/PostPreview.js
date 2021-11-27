779ebaa7a101ab7781fa88fd7decb0903cc4a8f1
rojekaleksandra <aleksandrar@student.agh.edu.pl>
2021-11-27 13:08:12 +0100

    task #6

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -1,6 +1,6 @@
-import { Container, Grid, Avatar, Typography } from '@mui/material'
+import { Container, Grid, Avatar, Typography } from '@mui/material';
 
-const PostPreview = ({name, imageSrc}) => {
+const PostPreview = ({ name, imageSrc, comment }) => {
     return (
         <Container >
             <Grid container spacing={0} rowSpacing ={2} alignItems='center' border={1} borderColor='darkgrey'>
@@ -8,16 +8,21 @@ const PostPreview = ({name, imageSrc}) => {
                     <Avatar sx={{ width: 60, height: 60 }}>{name[0]}</Avatar>
                 </Grid>
                 <Grid item xs={9}>
-                    <Typography variant="h6" component="h1">
+                    <Typography variant='h6' component='h1' fontFamily='Arial'>
                         {name}
                     </Typography>
                 </Grid>
                 <Grid item xs={12}>
-                    <img src={imageSrc} alt="bla" width="610px" height="550px"/>
+                    <img src={imageSrc} alt='bla' width='100%' height='100%'/>
+                </Grid>
+                <Grid item xs={12} marginLeft={1}>
+                    <Typography variant='body1' component='h1' fontFamily='Arial'>
+                        <b>{name}</b> {comment}
+                    </Typography>
                 </Grid>
             </Grid>
         </Container>
     );
-}
+};
 
 export default PostPreview;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import Stack from '@mui/material/Stack';
-import PostPreview from 'components/PostPreview'
+import PostPreview from 'components/PostPreview';
 
 const Home = () => {
     return (
         <Stack direction="row" spacing={2}>
             <Stack flex={1}>
                 {/* This is the place for story list and post list */}
-                <PostPreview name='uzytkownik' imageSrc="https://i2.wp.com/www.pies.pl/wp-content/uploads/pies-przekrzywia-glowe.jpg?fit=1400%2C800&ssl=1"/>
+                <PostPreview name='user' imageSrc="https://i2.wp.com/www.pies.pl/wp-content/uploads/pies-przekrzywia-glowe.jpg?fit=1400%2C800&ssl=1" comment="comment" />
             </Stack>
             <Stack width="300px">
                 {/* All Sidebar related things go here */}
