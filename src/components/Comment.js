bdee07287cdbe1320ca10428328c72b04e2c7dc9
qcianna <annaqcia@gmail.com>
2021-11-27 16:11:30 +0100

    next fixes comment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,30 +1,37 @@
 import React from 'react';
 import Avatar from '@mui/material/Avatar';
 import "./comment.css";
+import propTypes from 'prop-types';
 
 function Comment({userName, text, creationDate, imgUrl}) {
 
-    var day = creationDate.getDate();
-    var month = creationDate.getMonth() + 1;
-    var year = creationDate.getFullYear();
-    var h = creationDate.getHours();  
-    var m = creationDate.getMinutes(); 
+    const day = creationDate.getDate();
+    const month = creationDate.getMonth() + 1;
+    const year = creationDate.getFullYear();
+    const h = creationDate.getHours();  
+    const m = creationDate.getMinutes(); 
     
-
     return (
         <>
-            <div className="wrapper">
-                <Avatar alt="dog" src={imgUrl} />
+            <div className='wrapper'>
+                <Avatar alt='zdj' src={imgUrl} />
                 <div>
-                    <div className="wrapper">
-                        <p className="user">{userName}</p>
-                        <p className="text">{text}</p>
+                    <div className='wrapper'>
+                        <p className='user'>{userName}</p>
+                        <p className='text'>{text}</p>
                     </div>
-                    <p className="date">{`${day}-${month}-${year} ${h}h ${m}min`}</p>
+                    <p className='date'>{`${day}-${month}-${year} ${h}h ${m}min`}</p>
                 </div>
             </div>
         </>
     );
+}
+
+Comment.propTypes = {
+    userName: propTypes.string,
+    text: propTypes.string,
+    creationDate: propTypes.instanceOf(Date),
+    imgUrl: propTypes.string
 }
 
 export default Comment;
