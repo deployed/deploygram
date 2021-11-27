a6d103301032a4e93602914473b778954620041a
w3raza <weraza@vp.pl>
2021-11-27 16:04:02 +0100

    up-to-date

--- src/pages/SidebarFolder/Sidebar.js
+++ src/pages/SidebarFolder/Sidebar.js
@@ -1,9 +1,15 @@
+import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
 import profilePhoto from '../../images/cat.jpg';
-import './styles.css';
+
 import Footer from './footer.js';
+
 
 const Sidebar = () => {
     return (
@@ -21,35 +27,67 @@ const Sidebar = () => {
                 <Stack>
                     <Typography sx={{ fontWeight: 'bold', marginTop: '40px' }}>Suggestions for you</Typography>
                 </Stack>
+
+
                 
-                <Stack direction="row" spacing={2} alignItems="center">
-                    <Avatar
-                        alt="Remy Sharp"
-                        src = {profilePhoto}
-                        sx={{ width: 60, height: 60, borderRadius: 400/ 2 }} />
-                    <Typography sx={{ textAlign: 'center', fontWeight: 'bold' }}>Nazwa profilu</Typography>
-                </Stack>
-                <Stack direction="row" spacing={2} alignItems="center">
-                    <Avatar
-                        alt="Remy Sharp"
-                        src = {profilePhoto}
-                        sx={{ width: 60, height: 60, borderRadius: 400/ 2 }} />
-                    <Typography sx={{ textAlign: 'center', fontWeight: 'bold' }}>Nazwa profilu</Typography>
-                </Stack>
-                <Stack direction="row" spacing={2} alignItems="center">
-                    <Avatar
-                        alt="Remy Sharp"
-                        src = {profilePhoto}
-                        sx={{ width: 60, height: 60, borderRadius: 400/ 2 }} />
-                    <Typography sx={{ textAlign: 'center', fontWeight: 'bold' }}>Nazwa profilu</Typography>
-                </Stack>
-                <Stack direction="row" spacing={2} alignItems="center">
-                    <Avatar
-                        alt="Remy Sharp"
-                        src = {profilePhoto}
-                        sx={{ width: 60, height: 60, borderRadius: 400/ 2 }} />
-                    <Typography sx={{ textAlign: 'center', fontWeight: 'bold' }}>Nazwa profilu</Typography>
-                </Stack>
+                <List sx={{ width: '100%' }}>
+                    <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                            <Avatar
+                                alt="Remy Sharp"
+                                src = {profilePhoto}
+                                sx={{ width: 60, height: 60, borderRadius: 400/ 2 }} />
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary={
+                                <Typography sx={{ margin: '10px', marginTop: '20px',  fontWeight: 'bold' }}>Nazwa profilu
+                                </Typography>
+                            }
+                        />
+                    </ListItem>
+                    <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                            <Avatar
+                                alt="Remy Sharp"
+                                src = {profilePhoto}
+                                sx={{ width: 60, height: 60, borderRadius: 400/ 2 }} />
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary={
+                                <Typography sx={{ margin: '10px', marginTop: '20px',  fontWeight: 'bold' }}>Nazwa profilu
+                                </Typography>
+                            }
+                        />
+                    </ListItem>
+                    <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                            <Avatar
+                                alt="Remy Sharp"
+                                src = {profilePhoto}
+                                sx={{ width: 60, height: 60, borderRadius: 400/ 2 }} />
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary={
+                                <Typography sx={{ margin: '10px', marginTop: '20px',  fontWeight: 'bold' }}>Nazwa profilu
+                                </Typography>
+                            }
+                        />
+                    </ListItem>
+                    <ListItem alignItems="flex-start">
+                        <ListItemAvatar>
+                            <Avatar
+                                alt="Remy Sharp"
+                                src = {profilePhoto}
+                                sx={{ width: 60, height: 60, borderRadius: 400/ 2 }} />
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary={
+                                <Typography sx={{ margin: '10px', marginTop: '20px',  fontWeight: 'bold' }}>Nazwa profilu
+                                </Typography>
+                            }
+                        />
+                    </ListItem>
+                </List>
                 <Footer/>
             </Stack>
         </>
--- src/pages/SidebarFolder/footer.js
+++ src/pages/SidebarFolder/footer.js
@@ -6,7 +6,7 @@ const footer = () =>{
         <Stack>
             <Typography sx={{ textAlign: 'center' , marginTop: '60px', color: 'rgb(153,153,153)' }}>
                         Privacy Terms Contact
-                        <br/>
+                <br/>
                         &#169;
                         2021 Deploygram
             </Typography>      
