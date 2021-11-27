1285c770f9ab985d38b71c3b280e16eb6a070a11
BinarySoftware <mkmikolajek@icloud.com>
2021-11-27 11:24:00 +0100

    ???

---
+++ src/components/CarouselPhoto.js
@@ -0,0 +1,61 @@
+import PhotoPost from './PhotoPost';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css'; 
+import 'slick-carousel/slick/slick-theme.css';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
+const CarouselPhoto = ({ images }) => {
+    const chevronStyle = {
+        color: 'black',
+        scale: '2'
+    };
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: <SamplePrevArrow/>,
+        nextArrow: <SampleNextArrow/>,
+    };
+
+    const style = {
+        width: '625px',
+        height: '100%'
+    };
+
+    return (<Slider {...settings} style={style}>
+        {images.map((image, index) => {
+            return (<PhotoPost image={image} key={index}></PhotoPost>);
+        })
+        }
+    </Slider>
+    );
+};
+
+export default CarouselPhoto;
+
+
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: 'block', color: 'black', scale:'2' , left: '575px' }}
+            onClick={onClick}
+        />
+    );
+}
+  
+function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: 'block', color: 'black', scale:'2', left: '25px' }}
+            onClick={onClick}
+        />
+    );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
 import Stack from '@mui/material/Stack';
+import CarouselPhoto from 'components/CarouselPhoto';
 
 const Home = () => {
     return (
         <Stack direction="row" spacing={2}>
-            <Stack flex={1}>
+            <Stack flex={1} width="700px">
                 {/* This is the place for story list and post list */}
                 Timeline
+
+                <CarouselPhoto images={['https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
+                    'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png',
+                    'https://im0-tub-ru.yandex.net/i?id=84dbd50839c3d640ebfc0de20994c30d&n=27&h=480&w=480']}>
+                </CarouselPhoto>
             </Stack>
             <Stack width="300px">
                 {/* All Sidebar related things go here */}
